815ad5125ef86354dabc65f38dd8d5576d2a0a1a
felipefln <felipecarvalho_22_@hotmail.com>
2019-11-09 14:44:14 -0300

    Arquivo de iniciação do servidor

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+
+const http = require('http')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+const server = http.Server(app)
+
+//utilizado Mongoose como dependencia para ajudar na conexao entre NODEJS e MongoDB
+mongoose.connect('mongodb+srv://add-clientes:add-clientes@clientes-p2oc2.mongodb.net/clientes?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    keepAlive: true
+})
+.then(() => console.log('DB Conectado :)!'))
+.catch(err => {
+console.log(Error, err.message);
+});
+
+
+
+
+app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(express.json())
+app.use(routes)
+
+server.listen(Number(process.env.PORT) || 5000);
